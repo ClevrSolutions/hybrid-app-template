d184dc9f25ba7998d961fc69749390a8c09d002a
Edwin Huitema <58079667+ff-edwin@users.noreply.github.com>
2020-03-24 18:08:22 +0100

    ref updates to @flowfabric/mendix-hybrid-app-base

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 var webpack_merge = require('webpack-merge');
 
-var base_config = require("@mendix/mendix-hybrid-app-base/webpack.config");
+var base_config = require("@flowfabric/mendix-hybrid-app-base/webpack.config");
 
 module.exports = function(env) {
-    let config = webpack_merge(base_config(env), {
-    });
+    let config = webpack_merge(base_config(env), {});
 
     return config;
 };
